3943be17df9a4ae17561e8a939b342298426d3fb
Fr4nk1in <fushen@mail.ustc.edu.cn>
2026-03-16 22:05:01 +0800

    fix: installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,9 @@ REMODEV_BASE="$(dirname "$BASE_DIR")"
 
 export HOME="$REMODEV_BASE"
 
+echo "Installing dependencies..."
+apt-get install libatomic1 clang build-essential curl -y
+
 echo "Installing mise..."
 curl https://mise.run | MISE_INSTALL_PATH="$HOME/.local/bin/mise" sh
 eval "$("$HOME/.local/bin/mise" activate bash)"
