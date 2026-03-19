74a5a41755fc19d61fc11ca9b78d54cbf57d6608
Fr4nk1in <fushen@mail.ustc.edu.cn>
2026-03-19 18:05:45 +0800

    feat: colorscheme change

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -44,23 +44,12 @@ end
 
 
 require("lazy").setup({
-  ui = { border = "rounded" },
-  install = { colorscheme = "nordfox" },
   {
-    "EdenEast/nightfox.nvim",
+    "RRethy/base16-nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      groups = {
-        all = {
-          FloatBorder = { fg = "fg3", bg = "bg0" },
-          FloatTitle = { fg = "syntax.func", bg = "bg0" },
-        },
-      },
-    },
-    config = function(_, opts)
-      require("nightfox").setup(opts)
-      vim.cmd.colorscheme("nordfox")
+    config = function(_, _)
+      vim.cmd.colorscheme("base16-nord-light")
     end,
   },
   {
